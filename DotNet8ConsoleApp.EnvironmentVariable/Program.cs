abe6647273133c35f32c53d765419d11b0728f13
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-05 13:10:00 +0630

    Add project files.

---
+++ DotNet8ConsoleApp.EnvironmentVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNet8ConsoleApp.EnvironmentVariable;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var path =
+            @"C:\projects\Examples\DotNet8ConsoleApp.EnvironmentVariable\DotNet8ConsoleApp.EnvironmentVariable\.env";
+        DotNetEnv.Env.Load(path);
+
+        var message = Environment.GetEnvironmentVariable("MESSAGE");
+        Console.WriteLine($"MESSAGE: {message}");
+    }
+}
